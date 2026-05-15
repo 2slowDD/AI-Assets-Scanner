2e0e53063b9e1ba07ab522d8167ad2ca3c57d52b
CU Scanner Dev <dev@cu-scanner.local>
2026-05-15 19:27:26 +0200

    test(fu-new-2): StrategyFactory no longer instantiates FlyingPress strategy

--- tests/StrategyFactoryTest.php
+++ tests/StrategyFactoryTest.php
@@ -2,14 +2,13 @@
 namespace CUScanner\Tests;
 
 use CUScanner\Scanner\StrategyFactory;
-use CUScanner\Scanner\Strategies\FlyingPressBypass;
 
 class StrategyFactoryTest extends \PHPUnit\Framework\TestCase {
 
-    public function test_for_method_flying_press_returns_flying_press_strategy(): void {
-        $strategy = StrategyFactory::for_method( 'flying_press' );
-        $this->assertInstanceOf( FlyingPressBypass::class, $strategy );
-        $this->assertSame( 'flying_press', $strategy->slug() );
+    public function test_for_method_flying_press_throws_after_reclass(): void {
+        $this->expectException( \InvalidArgumentException::class );
+        $this->expectExceptionMessageMatches( '/Unknown disable_method: flying_press/' );
+        StrategyFactory::for_method( 'flying_press' );
     }
 
     public function test_for_method_unknown_throws(): void {
